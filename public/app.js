c866c4d153627be5f164701e99b02c2c21c9a9fa
Jack Hansen <hansenja2011@gmail.com>
2017-11-26 12:40:22 -0700

    Added pricing logic.

--- public/app.js
+++ public/app.js
@@ -1,17 +1,42 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
-    $('.thumbnail').click(function() {
+    $('.thumbnail').click(function () {
 
         var image = $(this).attr('src');
-        
+
         $('#mainPic').attr('src', image);
     });
 
-    var price = 0;
+    var original = 100;
+    var totalPrice = original;
+    var addonPrice = 0;
 
-    $('')
 
-    
+    changePrice(original);
+
+    $('#options').on('change', function () {
+
+        var value = $(this).val();
+        addonPrice = parseInt(value);
+
+        $('#quantity').val(1);
+
+        changePrice(original + addonPrice);
+    });
+
+    $('#quantity').on('change', function () {
+
+        var value = $(this).val();
+
+        var quantityPrice = original * parseInt(value);
+        totalPrice = (parseInt(value) * addonPrice) + quantityPrice;
+
+        changePrice(totalPrice);
+    })
+
+    function changePrice(price) {
+        $('#itemPrice').text("Price: $" + price);
+    }
+
+
 });
-
-
